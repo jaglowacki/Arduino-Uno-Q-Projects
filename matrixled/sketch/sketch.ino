0a5db6add47b86808476963e9ea5ad764fdfa836
Janusz Głowacki <jglowacki@int.pl>
2026-01-09 13:13:42 +0000

    added moving led

--- matrixled/sketch/sketch.ino
+++ matrixled/sketch/sketch.ino
@@ -10,6 +10,7 @@ uint8_t logo[104] = {
     0,1,0,0,0,1,0,1,0,0,0,1,0,
     0,0,1,1,1,0,0,0,1,1,1,0,0
 };
+uint8_t led_array[104];
 
 Arduino_LED_Matrix matrix;
 
@@ -18,7 +19,16 @@ void setup() {
   // display the image
   matrix.setGrayscaleBits(2);
   matrix.draw(logo);
-
+  
+  delay(1000);
+  for(int led_no=0; led_no < sizeof(led_array); led_no++){
+     led_array[led_no] = 3;
+     matrix.draw(led_array);
+     delay(100);
+     led_array[led_no] = 0;
+     matrix.draw(led_array);
+   } 
+  matrix.draw(logo);
 }
 
 void loop() {
